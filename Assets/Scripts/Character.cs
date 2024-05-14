bc516c61f301282abe9763cd771a294c90f659b4
maple-rain <demonpanda1369@gmail.com>
2024-05-14 11:49:38 +0900

    선택기능 추가

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer SpriteRenderer;
+    private Animator animator;
+
+    [SerializeField]
+    private RuntimeAnimatorController firstcontroller;
+    [SerializeField]
+    private RuntimeAnimatorController secondcontroller;
+    public void Awake()
+    {
+        SpriteRenderer.sprite = Resources.Load<Sprite>($"Character{PlayerPrefs.GetInt("CharacterType")}");
+        animator = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        Anim();
+    }
+    public void Anim()
+    {
+        int anime = PlayerPrefs.GetInt("CharacterType");
+        if ( anime == 0 ) 
+        {
+            animator.runtimeAnimatorController = firstcontroller;
+        }
+        else
+        {
+            animator.runtimeAnimatorController = secondcontroller;
+        }
+    }
+
+}
--- Assets/Scripts/LogIn.cs
+++ Assets/Scripts/LogIn.cs
@@ -11,8 +11,13 @@ public class LogIn : MonoBehaviour
 {
     [SerializeField]
     private TMP_InputField nameInputField;
+    [SerializeField]
+    private GameObject characterSelectCanvas;
+    [SerializeField]
+    private Image charImg;
+
     
-        
+
     public void LogInBtn()
     {
         string playerName = nameInputField.text;
@@ -24,6 +29,31 @@ public class LogIn : MonoBehaviour
 
             SceneManager.LoadScene("MainScene");
         }
+
     }
+
+    public void ChangeCharacterBtn()
+    {
+        characterSelectCanvas.SetActive(true);
+        
+    }
+
+    public void KnightSelectBtn()
+    {
+        PlayerPrefs.SetInt("CharacterType", 0);
+        characterSelectCanvas.SetActive(false);
+        charImg.sprite = Resources.Load<Sprite>("Character0");
+    }
+
+    public void WizardSelcetBtn()
+    {
+        PlayerPrefs.SetInt("CharacterType", 1);
+        characterSelectCanvas.SetActive(false);
+        charImg.sprite = Resources.Load<Sprite>("Character1");
+    }
+
+
    
+    
 }
+ 
